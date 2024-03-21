3545fcd281214632ca2b3fbf724d147e539127cc
Dilmurod Rozikov <2110115@newuzbekistanuniversity.uz>
2024-03-21 09:49:49 +0500

    Initialize local mssql

--- ParkingZoneApp/Program.cs
+++ ParkingZoneApp/Program.cs
@@ -16,7 +16,7 @@ namespace ParkingZoneApp
                 options.UseSqlServer(connectionString));
             builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
-            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             builder.Services.AddControllersWithViews();
 
