659d1497a384d7a05c03bf1114043ad503855a43
Dilmurod Rozikov <dilmurodrozikovvv@gmail.com>
2024-07-17 18:43:59 +0500

    done

---
+++ ParkingZoneApp/Data/Migrations/20240712133907_addNewPropertyToReservarion.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ParkingZoneApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class addNewPropertyToReservarion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsPaid",
+                table: "Reservation",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsPaid",
+                table: "Reservation");
+        }
+    }
+}
--- ParkingZoneApp/Models/Entities/Payment.cs
+++ ParkingZoneApp/Models/Entities/Payment.cs
@@ -4,13 +4,11 @@ using System.ComponentModel.DataAnnotations.Schema;
 namespace ParkingZoneApp.Models.Entities
 {
     [Table("PaymentDetails")]
+    [ComplexType]
     public class Payment
     {
         [Required]
         public Guid Id { get; set; }
-
-        [Required]
-        public bool PaymentStatus { get; set; }
 
         [Required]
         [RegularExpression(@"^\d{16}$", ErrorMessage = "Card number must be 16 digits.")]
@@ -28,7 +26,7 @@ namespace ParkingZoneApp.Models.Entities
         public string CVV { get; set; }
 
         [Required]
-        public DateOnly ExpirationDate { get; set; } = new(DateTime.Now.Year - 5, 1, 1);
+        public DateOnly ExpirationDate { get; set; } = new(DateTime.Now.Year, DateTime.Now.Month, 1);
 
         [Required]
         public virtual ParkingSlot ParkingSlot { get; set; }
